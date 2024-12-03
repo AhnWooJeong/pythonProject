f3fb2dcdabce03439e35b9510b76fb2855050d34
ITSC <itsc@example.com>
2024-12-03 21:48:39 +0900

    section12

---
+++ section12/converter.py
@@ -0,0 +1,30 @@
+'''
+파일 명: converter.py
+'''
+
+MILES = 0.621371
+POUND = 0.002220462
+
+def kilometer_to_miles(kilometer):
+    return kilometer * MILES
+
+def gram_to_pound(gram):
+    return gram * POUND
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
