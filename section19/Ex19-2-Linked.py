83760933caa5b92c19c9d36f80befa6752760830
ITSC <itsc@example.com>
2024-12-17 21:28:27 +0900

    section19

--- section19/Ex19-2-Linked.py
+++ section19/Ex19-2-Linked.py
@@ -29,3 +29,52 @@ class LinkedList:
             current = current.next
 
         current.next = new_node
+
+    def insert_node(self, find_data, insert_data):
+        if self.head is None:
+            return
+
+        if self.head.data == find_data:
+            self.head = Node(insert_data, self.head)
+
+        current = self.head
+        while current.next is not None:
+            if current.next.data == find_data:
+                new_node = Node(insert_data, current.next)
+                current.next = new_node
+                return
+            current = current.next
+
+        self.add_node(insert_data)
+
+    def delete_node(self, del_data):
+        if self.head is None:
+            return
+
+        if self.head.data == del_data:
+            self.head = self.head.next
+            return
+
+        current = self.head
+
+        while current.next is not None:
+            if current.next.data == del_data:
+                current.next = current.next.next
+                return
+            current = current.next
+
+    def print_list(self):
+        current = self.head
+        while current is not None:
+            print(current.data, end=' ')
+            current = current.next
+
+# 실행코드 73916
+linked_list = LinkedList()
+linked_list.add_node(7)
+linked_list.add_node(3)
+linked_list.add_node(9)
+linked_list.add_node(1)
+linked_list.add_node(6)
+
+linked_list.print_list()
